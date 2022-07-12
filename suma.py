eb93e58d14fc43984de826c53c05588b7437c8ab
JUAHISAN <63412860+JUAHISAN@users.noreply.github.com>
2022-07-12 20:52:11 +0200

    Cheat Sheet PySpark

--- suma.py
+++ suma.py
@@ -1,18 +1,25 @@
 import pyspark
 from pyspark import SparkContext
+import time
 
 sc = SparkContext()
 
-numeros = sc.parallelize([1,2,3,4,5,6,7,8,9,10])
-sum_numeros = numeros.map(lambda a: a+10).collect()
-print(sum_numeros)
+inicio = time.time()
+#numeros = sc.parallelize(range(1000000))
+#numeros_sc = sc.parallelize([1,2,3,4,5,6,7,8,9,10])
+#sum_numeros = numeros.map(lambda a: a+10).collect()
+#print(sum_numeros)
 
-numeros_2 = [1,2,3,4,5,6,7,8,9,10]
+numeros_2 = range(1000000)
 lista = list()
 
 for i in numeros_2:
     a = i+10
-    print (a)
+#    #print (a)
     lista.append(a)
 
-print(lista)
+#print(lista)
+
+
+fin = time.time()
+print("Total tiempo ejecución {}".format(fin-inicio)) 
